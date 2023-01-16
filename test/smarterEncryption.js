3c7b033dfc957d8bfba421361d59922b680380b1
Sam Macbeth <smacbeth@duckduckgo.com>
2023-01-16 18:08:41 +0100

    Reorder createSmarterEncryptionExceptionRule args

--- test/smarterEncryption.js
+++ test/smarterEncryption.js
@@ -280,7 +280,7 @@ describe('generateSmarterEncryptionRuleset', () => {
     it('createSmarterEncryptionExceptionRule should prevent https upgrade for domain', async function () {
         const testDomains = ['privacy-test-pages.glitch.me', 'glitch.me']
         await this.browser.addRules(generateSmarterEncryptionRuleset(testDomains, 2))
-        await this.browser.addRules([createSmarterEncryptionExceptionRule([testDomains[0]], 1).rule])
+        await this.browser.addRules([createSmarterEncryptionExceptionRule([testDomains[0]], 'allow', 1).rule])
         const expectNotUpgraded = await this.browser.testMatchOutcome({
             url: 'http://privacy-test-pages.glitch.me/insecure',
             type: 'main_frame',
